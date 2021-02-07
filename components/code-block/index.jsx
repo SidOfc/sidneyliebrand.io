3a3ceba9d05ef1c3fd99ec587fd2072a4e89ec6d
Sidney Liebrand <sidneyliebrand@gmail.com>
2021-02-08 00:37:26 +0100

    try a different logo, use shell language consistently in md

--- components/code-block/index.jsx
+++ components/code-block/index.jsx
@@ -1,9 +1,21 @@
 import Highlight, {defaultProps} from 'prism-react-renderer';
 
+const THEME = {
+    plain: JSON.parse(JSON.stringify(defaultProps.theme.plain)),
+    styles: JSON.parse(JSON.stringify(defaultProps.theme.styles)),
+};
+
+THEME.styles.push({
+    language: 'bash',
+    style: {color: '#fa60c3'},
+    types: ['function'],
+});
+
 export default function CodeBlock({className, children}) {
     return (
         <Highlight
             {...defaultProps}
+            theme={THEME}
             className={className}
             code={children.props.children.trim()}
             language={children.props.className.split('-').pop()}
