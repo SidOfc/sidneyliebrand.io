946fe6a3a414d84f621417049a86f5205eadfa07
Sidney Liebrand <sidneyliebrand@gmail.com>
2021-02-13 00:07:40 +0100

    lazy load images with fade effect with react-lazyload

--- components/image/index.jsx
+++ components/image/index.jsx
@@ -1,22 +1,29 @@
 import {forwardRef} from 'react';
 import styles from './image.module.scss';
+import LazyLoad from 'react-lazyload';
 import {classes} from '../../util';
 
 export default forwardRef(
     ({src, width, height, className, alt, showAlt = true}, ref) => {
         const w = parseInt(width) || 1;
         const h = parseInt(height) || 0;
+        const paddingBottom = `${(h / w) * 100}%`;
 
         return (
             <figure className={classes(className, styles.figure)} ref={ref}>
-                <div
-                    className={styles.image}
-                    title={alt}
-                    style={{
-                        backgroundImage: `url(${src})`,
-                        paddingBottom: `${(h / w) * 100}%`,
-                    }}
-                />
+                <LazyLoad
+                    offset={200}
+                    placeholder={<div style={{paddingBottom}} />}
+                    once
+                >
+                    <div
+                        className={styles.image}
+                        style={{
+                            backgroundImage: `url(${src})`,
+                            paddingBottom,
+                        }}
+                    />
+                </LazyLoad>
                 {alt && showAlt && (
                     <figcaption className={styles.alt}>{alt}</figcaption>
                 )}
