ed901e4e24ea3085d4b97371dadaa8cb49ea4fb1
Sidney Liebrand <sidneyliebrand@gmail.com>
2021-02-16 03:47:03 +0100

    fix no html being generated

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,19 +1,10 @@
-import {useState, useEffect} from 'react';
 import '@styles/application.scss';
 import {Layout} from '@components/layout';
 
 export default function Application({Component, pageProps}) {
-    const [isMounted, setIsMounted] = useState(false);
-
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
-
     return (
-        isMounted && (
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        )
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
     );
 }
