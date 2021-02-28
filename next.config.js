72e215ed4918f0215225b8794bb72dff62284076
Sidney Liebrand <sidneyliebrand@gmail.com>
2021-02-28 17:46:33 +0100

    remove unnecessary webpack config option from next.config.js

--- next.config.js
+++ next.config.js
@@ -5,19 +5,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const sassVars = require('./data/sass-variables.json');
 
 module.exports = withBundleAnalyzer({
-    webpack(config, {isServer}) {
-        // https://coderberry.com/posts/mdx_next
-        // fixes import [module] from [node_lib] errors
-        // related to next-mdx-remote
-        if (!isServer) {
-            config.node = {
-                fs: 'empty',
-                path: 'empty',
-            };
-        }
-
-        return config;
-    },
     sassOptions: {
         prependData: `$vars: ${serializeToSass(sassVars)};`,
         functions: {
