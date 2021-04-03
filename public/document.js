f393f1e3d93d96d43a1208e9b0ff354cb980c1a7
Sidney Liebrand <sidneyliebrand@gmail.com>
2021-04-03 07:33:07 +0200

    site theme updates all tabs on change, when JS is disabled theme uses OS preference, right-click removes theme preference (uses OS preference)

--- public/document.js
+++ public/document.js
@@ -4,53 +4,63 @@
     var d = document;
     var html = d.querySelector('html');
     var style = d.createElement('style');
+    var theme;
+
+    try {
+        theme = localStorage.getItem('theme');
+    } catch (e) {}
+
+    function applyTheme(theme, store) {
+        themeNames.forEach(function (name) {
+            html.classList.remove(name);
+        });
+
+        if (theme) {
+            html.classList.add(theme);
+            style.innerHTML =
+                'html { background-color:' + themeBackgrounds[theme] + '; }';
+        }
+
+        if (store) {
+            try {
+                if (theme) {
+                    localStorage.setItem('theme', theme);
+                } else {
+                    localStorage.removeItem('theme');
+                }
+            } catch (e) {}
+        }
+    }
 
     html.classList.remove('no-js');
     d.head.appendChild(style);
 
-    function applyTheme(theme, store) {
-        if (theme) {
-            themeNames.forEach(function (name) {
-                html.classList.remove(name);
-            });
-
-            html.classList.add(theme);
-            style.innerHTML =
-                'html { background-color:' + themeBackgrounds[theme] + '; }';
-
-            if (store) {
-                try {
-                    localStorage.setItem('theme', theme);
-                } catch (e) {}
-            }
+    window.addEventListener('contextmenu', function (ev) {
+        if (ev.target.getAttribute('data-theme')) {
+            ev.preventDefault();
+            applyTheme(null, true);
         }
+    });
+
+    window.addEventListener('click', function (ev) {
+        applyTheme(ev.target.getAttribute('data-theme'), true);
+    });
+
+    window.addEventListener('storage', function (ev) {
+        if (ev.key === 'theme') applyTheme(ev.newValue);
+    });
+
+    if (window.matchMedia) {
+        var darkThemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+        if (!theme && darkThemeQuery.matches) theme = 'dark';
+
+        darkThemeQuery.addEventListener('change', function (query) {
+            if (!localStorage.getItem('theme')) {
+                applyTheme(query.matches ? 'dark' : 'light');
+            }
+        });
     }
 
-    window.addEventListener(
-        'click',
-        function (ev) {
-            applyTheme(ev.target.getAttribute('data-theme'), true);
-        },
-        {passive: true}
-    );
-
-    try {
-        var theme = localStorage.getItem('theme');
-
-        if (window.matchMedia) {
-            var darkThemeQuery = window.matchMedia(
-                '(prefers-color-scheme: dark)'
-            );
-
-            if (!theme && darkThemeQuery.matches) theme = 'dark';
-
-            darkThemeQuery.addEventListener('change', function (query) {
-                if (!localStorage.getItem('theme')) {
-                    applyTheme(query.matches ? 'dark' : 'light');
-                }
-            });
-        }
-
-        applyTheme(theme);
-    } catch (e) {}
+    applyTheme(theme);
 })();
