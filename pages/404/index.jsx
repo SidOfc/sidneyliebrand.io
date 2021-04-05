d8ec0023d686e8504ea25d323945f7ecb47e7796
Sidney Liebrand <sidneyliebrand@gmail.com>
2021-04-05 20:16:53 +0200

    add link to view homepage button on 404 page, remove runtime js from 404 page

--- pages/404/index.jsx
+++ pages/404/index.jsx
@@ -2,6 +2,8 @@ import styles from './404.module.scss';
 import Media from '@components/media';
 import Button from '@components/button';
 
+export const config = {unstable_JsPreload: false, unstable_runtimeJS: false};
+
 export default function PageNotFound() {
     return (
         <section className={styles.page}>
@@ -13,7 +15,7 @@ export default function PageNotFound() {
                 className={styles.jackieChanWhat}
             />
             <h1 className={styles.pageHeading}>Page not found, what?!</h1>
-            <Button>View homepage</Button>
+            <Button href="/">View homepage</Button>
         </section>
     );
 }
