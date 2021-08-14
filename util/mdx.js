da2340a3c6a6db02a4e63a1e97ebaed110c4a838
Sidney Liebrand <sidneyliebrand@gmail.com>
2021-08-14 16:36:58 +0200

    update packages, fix linter error

--- util/mdx.js
+++ util/mdx.js
@@ -8,11 +8,23 @@ export const MARKDOWN_OPTIONS = {
         Media,
         Caniuse,
         pre: CodeBlock,
-        h1: (props) => <Heading {...props} as="h1" />,
-        h2: (props) => <Heading {...props} as="h2" />,
-        h3: (props) => <Heading {...props} as="h3" />,
-        h4: (props) => <Heading {...props} as="h4" />,
-        h5: (props) => <Heading {...props} as="h5" />,
-        h6: (props) => <Heading {...props} as="h6" />,
+        h1(props) {
+            return <Heading {...props} as="h1" />;
+        },
+        h2(props) {
+            return <Heading {...props} as="h2" />;
+        },
+        h3(props) {
+            return <Heading {...props} as="h3" />;
+        },
+        h4(props) {
+            return <Heading {...props} as="h4" />;
+        },
+        h5(props) {
+            return <Heading {...props} as="h5" />;
+        },
+        h6(props) {
+            return <Heading {...props} as="h6" />;
+        },
     },
 };
